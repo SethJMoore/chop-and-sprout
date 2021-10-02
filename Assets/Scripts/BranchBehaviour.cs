75abed47d114160c2f4490d6a916b251058a1bfd
Seth J. Moore <seth@sethjmoore.com>
2021-10-02 12:32:50 -0400

    Removed unneeded variable

--- Assets/Scripts/BranchBehaviour.cs
+++ Assets/Scripts/BranchBehaviour.cs
@@ -6,13 +6,11 @@ public class BranchBehaviour : MonoBehaviour
 {
     private float growthSpeed = 0.001f;
     private Vector3 scaleChange;
-    private Transform trans;
 
     // Start is called before the first frame update
     void Start()
     {
         scaleChange = new Vector3(0, growthSpeed, 0);
-        //trans = gameObject.transform;
     }
 
     // Update is called once per frame
