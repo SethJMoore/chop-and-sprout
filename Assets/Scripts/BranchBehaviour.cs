dd3e23412e6ac35bf370055f17bfb265eb854792
Seth J. Moore <seth@sethjmoore.com>
2021-10-02 17:34:04 -0400

    Took different approach and got branches growing.

--- Assets/Scripts/BranchBehaviour.cs
+++ Assets/Scripts/BranchBehaviour.cs
@@ -4,23 +4,57 @@ using UnityEngine;
 
 public class BranchBehaviour : MonoBehaviour
 {
-    private float growthSpeed = 0.001f;
-    private Vector3 scaleChange;
+    [SerializeField]
+    private GameObject segment;
+    [SerializeField]
+    private float timeBetweenNewSegments = 1f;
+    private GameObject lastSegment;
+    private Transform lastSegmentTransform;
+    private float timeSinceLastSegment = 0;
+    private List<GameObject> listOfSegments = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        scaleChange = new Vector3(0, growthSpeed, 0);
+        lastSegmentTransform = transform;
+        addSegment();
     }
 
     // Update is called once per frame
     void Update()
     {
-        grow();
+        if (timeSinceLastSegment >= timeBetweenNewSegments)
+        {
+            addSegment();
+            timeSinceLastSegment = 0;
+        }
+        else
+        {
+            timeSinceLastSegment += Time.deltaTime;
+        }
     }
 
-    void grow()
+    void addSegment()
     {
-        transform.localScale += scaleChange;
+        //lastSegment = Instantiate(segment, lastSegmentTransform);
+        if (lastSegment)
+        {
+            lastSegment = Instantiate(segment, transform);
+            lastSegment.transform.localPosition = lastSegmentTransform.localPosition + Vector3.down * 0.05f;
+            lastSegmentTransform = lastSegment.transform;
+            listOfSegments.Add(lastSegment);
+        }
+        else
+        {
+            lastSegment = Instantiate(segment, transform);
+            lastSegment.transform.position = lastSegmentTransform.localPosition + Vector3.down * 0.05f;
+            lastSegmentTransform = lastSegment.transform;
+            listOfSegments.Add(lastSegment);
+        }
+    }
+
+    Quaternion randomRotation()
+    {
+        return Quaternion.Euler(0.0f, 0.0f, Random.Range(0, 360));
     }
 }
